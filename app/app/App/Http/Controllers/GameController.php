a0f1ab23a63a6d531d7df2c2361527b360f25933
Sean Busse <schuelerst@yahoo.de>
2024-07-06 10:54:20 +0000

    preventing consecutive turns

--- app/app/App/Http/Controllers/GameController.php
+++ app/app/App/Http/Controllers/GameController.php
@@ -131,6 +131,8 @@ class GameController extends Controller
         // GamePlayer::Human (the last move was made by the player) or GamePlayer::None (this is the first move).
         // Inside of $player you have the player which wants to play now.
         // If he is allowed to play, you have to return true, otherwise you have to return false.
+        if ( $game->getLastPlayer() === $player )
+            return false;
 
         return true;
     }
